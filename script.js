2ac47a213333d57e12fc8af79e42aaaf65d5be22
Bruce E. Sutherland <102152491+BruceVFS@users.noreply.github.com>
2023-08-07 14:33:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,42 +6,42 @@ document.addEventListener('DOMContentLoaded', function() {
   // Attach an event listener to the button to trigger the search
   document.querySelector('button').addEventListener('click', searchCSV);
 
-  function searchCSV() {
-    const searchText = searchInput.value.toLowerCase();
+function searchCSV() {
+  const searchText = searchInput.value.toLowerCase();
 
-    // Fetch the CSV file using AJAX
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          const csvData = xhr.responseText;
+  // Fetch the CSV file using AJAX
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        const csvData = xhr.responseText;
 
-          // Parse the CSV data
-          const lines = csvData.split('\n');
-          const headers = lines[0].split(',');
-          const searchFields = ['product_code', 'serial_number', 'product_description'];
-          const results = [];
+        // Parse the CSV data
+        const lines = csvData.split('\n');
+        const headers = lines[0].split(','); // Ensure correct CSV parsing here
+        const searchFields = ['product_code', 'serial_number', 'product_description'];
+        const results = [];
 
-          for (let i = 1; i < lines.length; i++) {
-            const values = lines[i].split(',');
+        for (let i = 1; i < lines.length; i++) {
+          const values = lines[i].split(',');
 
-            // Check if the search text matches any of the search fields
-            if (searchFields.some(field => values[headers.indexOf(field)].toLowerCase().includes(searchText))) {
-              const entry = {};
-              for (let j = 0; j < headers.length; j++) {
-                entry[headers[j]] = values[j];
-              }
-              results.push(entry);
+          // Check if the search text matches any of the search fields
+          if (searchFields.some(field => headers.includes(field) && values[headers.indexOf(field)].toLowerCase().includes(searchText))) {
+            const entry = {};
+            for (let j = 0; j < headers.length; j++) {
+              entry[headers[j]] = values[j];
             }
+            results.push(entry);
           }
-
-          // Display the search results
-          displayResults(results);
-        } else {
-          console.error('Failed to fetch the CSV file.');
         }
+
+        // Display the search results
+        displayResults(results);
+      } else {
+        console.error('Failed to fetch the CSV file.');
       }
-    };
+    }
+  };
 
     // Replace 'search_sample.csv' with the actual path to your CSV file
     xhr.open('GET', 'search_sample.csv', true);
